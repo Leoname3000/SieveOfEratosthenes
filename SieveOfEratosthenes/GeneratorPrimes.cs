c12152af7ba4d20139fd02f8e7a87e0136cf248f
Leonid Sulin <sulin.leonid@gmail.com>
2024-04-07 12:57:43 +0300

    inline and rename counter variables

--- SieveOfEratosthenes/GeneratorPrimes.cs
+++ SieveOfEratosthenes/GeneratorPrimes.cs
@@ -9,40 +9,38 @@ public class GeneratorPrimes
             // Объявления
             int initialSize = maxValue + 1; // Размер массива
             Boolean[] isPrime = new bool[initialSize];
-            int i;
 
             //Инициализировать массив значением true
-            for (i = 0; i < initialSize; i++)
-                isPrime[i] = true;
+            for (int number = 0; number < initialSize; number++)
+                isPrime[number] = true;
 
             //Удалить числа, заведомо не являющиеся простыми
             isPrime[0] = isPrime[1] = false;
 
             //Отсев
-            int j;
-            for (i = 2; i < Math.Sqrt(initialSize) + 1; i++)
+            for (int number = 2; number < Math.Sqrt(initialSize) + 1; number++)
             {
-                if (isPrime[i]) //Если элемент i не вычеркнут, вычеркнуть кратные ему
+                if (isPrime[number]) //Если элемент i не вычеркнут, вычеркнуть кратные ему
                 {
-                    for (j = 2 * i; j < initialSize; j += i)
-                        isPrime[j] = false; //Кратные числа не являются простыми
+                    for (int multiple = 2 * number; multiple < initialSize; multiple += number)
+                        isPrime[multiple] = false; //Кратные числа не являются простыми
                 }
             }
 
             //Сколько простых чисел осталось?
             int primesSize = 0;
-            for (i = 0; i < initialSize; i++)
+            for (int number = 0; number < initialSize; number++)
             {
-                if (isPrime[i])
+                if (isPrime[number])
                     primesSize++; //Приращение счетчика
             }
 
             int[] primes = new int[primesSize];
             //Переместить простые числа в результат
-            for (i = 0, j = 0; i < initialSize; i++)
+            for (int number = 0, j = 0; number < initialSize; number++)
             {
-                if (isPrime[i]) //Если простое
-                    primes[j++] = i;
+                if (isPrime[number]) //Если простое
+                    primes[j++] = number;
             }
             return primes; //Вернуть простые числа
         }
