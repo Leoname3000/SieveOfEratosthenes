53442cb5fe753206960317408aedac1c7a620410
Leonid Sulin <sulin.leonid@gmail.com>
2024-04-07 13:19:55 +0300

    inline initialSize variable

--- SieveOfEratosthenes/GeneratorPrimes.cs
+++ SieveOfEratosthenes/GeneratorPrimes.cs
@@ -6,30 +6,29 @@ public class GeneratorPrimes
     {
         if (maxValue < 2)
             return Array.Empty<int>();
-
-        int initialSize = maxValue + 1;
-        Boolean[] isPrime = new bool[initialSize];
+        
+        Boolean[] isPrime = new bool[maxValue + 1];
         Array.Fill(isPrime, true);
         isPrime[0] = isPrime[1] = false;
 
-        for (int number = 2; number < Math.Sqrt(initialSize) + 1; number++)
+        for (int number = 2; number < Math.Sqrt(maxValue + 1) + 1; number++)
         {
             if (isPrime[number])
             {
-                for (int multiple = 2 * number; multiple < initialSize; multiple += number)
+                for (int multiple = 2 * number; multiple <= maxValue; multiple += number)
                     isPrime[multiple] = false;
             }
         }
 
         int primesSize = 0;
-        for (int number = 0; number < initialSize; number++)
+        for (int number = 0; number <= maxValue; number++)
         {
             if (isPrime[number])
                 primesSize++;
         }
 
         int[] primes = new int[primesSize];
-        for (int number = 0, j = 0; number < initialSize; number++)
+        for (int number = 0, j = 0; number <= maxValue; number++)
         {
             if (isPrime[number])
                 primes[j++] = number;
