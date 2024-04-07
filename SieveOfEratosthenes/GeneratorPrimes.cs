7bd113ae0587c44401e6765a528c3fec53035be7
Leonid Sulin <sulin.leonid@gmail.com>
2024-04-07 13:11:03 +0300

    replace if statement

--- SieveOfEratosthenes/GeneratorPrimes.cs
+++ SieveOfEratosthenes/GeneratorPrimes.cs
@@ -4,47 +4,45 @@ public class GeneratorPrimes
 {
     public static int[] generatePrimes(int maxValue)
     {
-        if (maxValue >= 2) // Единственно допустимый случай
+        if (maxValue < 2)
+            return Array.Empty<int>();
+
+        // Объявления
+        int initialSize = maxValue + 1; // Размер массива
+        Boolean[] isPrime = new bool[initialSize];
+
+        //Инициализировать массив значением true
+        for (int number = 0; number < initialSize; number++)
+            isPrime[number] = true;
+
+        //Удалить числа, заведомо не являющиеся простыми
+        isPrime[0] = isPrime[1] = false;
+
+        //Отсев
+        for (int number = 2; number < Math.Sqrt(initialSize) + 1; number++)
         {
-            // Объявления
-            int initialSize = maxValue + 1; // Размер массива
-            Boolean[] isPrime = new bool[initialSize];
-
-            //Инициализировать массив значением true
-            for (int number = 0; number < initialSize; number++)
-                isPrime[number] = true;
-
-            //Удалить числа, заведомо не являющиеся простыми
-            isPrime[0] = isPrime[1] = false;
-
-            //Отсев
-            for (int number = 2; number < Math.Sqrt(initialSize) + 1; number++)
+            if (isPrime[number]) //Если элемент i не вычеркнут, вычеркнуть кратные ему
             {
-                if (isPrime[number]) //Если элемент i не вычеркнут, вычеркнуть кратные ему
-                {
-                    for (int multiple = 2 * number; multiple < initialSize; multiple += number)
-                        isPrime[multiple] = false; //Кратные числа не являются простыми
-                }
+                for (int multiple = 2 * number; multiple < initialSize; multiple += number)
+                    isPrime[multiple] = false; //Кратные числа не являются простыми
             }
-
-            //Сколько простых чисел осталось?
-            int primesSize = 0;
-            for (int number = 0; number < initialSize; number++)
-            {
-                if (isPrime[number])
-                    primesSize++; //Приращение счетчика
-            }
-
-            int[] primes = new int[primesSize];
-            //Переместить простые числа в результат
-            for (int number = 0, j = 0; number < initialSize; number++)
-            {
-                if (isPrime[number]) //Если простое
-                    primes[j++] = number;
-            }
-            return primes; //Вернуть простые числа
         }
-        else //maxValue < 2
-            return new int[0]; //Вернуть пустой массив
+
+        //Сколько простых чисел осталось?
+        int primesSize = 0;
+        for (int number = 0; number < initialSize; number++)
+        {
+            if (isPrime[number])
+                primesSize++; //Приращение счетчика
+        }
+
+        int[] primes = new int[primesSize];
+        //Переместить простые числа в результат
+        for (int number = 0, j = 0; number < initialSize; number++)
+        {
+            if (isPrime[number]) //Если простое
+                primes[j++] = number;
+        }
+        return primes; //Вернуть простые числа
     }
 }
