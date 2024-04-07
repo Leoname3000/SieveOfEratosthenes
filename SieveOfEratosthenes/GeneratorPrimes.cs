c9ef5717d6ceecfae4aa5c23f5a9edd9adcaa101
Leonid Sulin <sulin.leonid@gmail.com>
2024-04-07 13:31:08 +0300

    remove redundant braces

--- SieveOfEratosthenes/GeneratorPrimes.cs
+++ SieveOfEratosthenes/GeneratorPrimes.cs
@@ -12,20 +12,15 @@ public class GeneratorPrimes
         isPrime[0] = isPrime[1] = false;
 
         for (int number = 2; number < Math.Sqrt(maxValue + 1) + 1; number++)
-        {
             if (isPrime[number])
-            {
                 for (int multiple = 2 * number; multiple <= maxValue; multiple += number)
                     isPrime[multiple] = false;
-            }
-        }
 
         List<int> primes = new List<int>();
         for (int number = 0; number <= maxValue; number++)
-        {
             if (isPrime[number])
                 primes.Add(number);
-        }
+        
         return primes.ToArray();
     }
 }
